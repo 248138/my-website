98c5b559a7f5e2490ff71db06b8995c6a47c0481
248138 <248138@portlandschools.org>
2026-09-18 14:46:28 -0400

    added index.js

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+// console.log('Name: Orion')
+// console.log('Age: 15')
+// console.warn("Warning: Don't forget to take your fish for a walk")
+// console.error("Error: Failed to load the part that needs to be loaded")
+
+// console.log('string')
+// console.log(10)
+// console.log(true)
+// console.log([1,2,3])
+// console.log(typeof 10.5, typeof 'hi', typeof true, typeof [1,2,3])
+
+// const schoolName = 'Portland High School'
+// let middleSchool = 'King Middle School'
+// console.log(schoolName)
+// console.log(middleSchool)
+
+// middleSchool = 'Lyman Moore Middle School'
+
+// console.log(schoolName)
+// console.log(middleSchool)
+
+// alert('sorry')
+
+const user_name = 'Orion'//prompt('Name:')
+const favorite_food = 'Pizza'//prompt('Favorite food:')
+
+console.log('---- User Profile ----')
+console.log('Name:', user_name)
+console.log("Favorite Food:", favorite_food)
+
+// alert('Thanks '+user_name+', enjoy')
