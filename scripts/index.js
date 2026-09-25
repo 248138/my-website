ce521d71cf81e00eb021a4b5c3877ab9d3654314
248138 <248138@portlandschools.org>
2026-09-25 14:49:00 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,16 @@
-// console.log('Name: Orion')
-// console.log('Age: 15')
-// console.warn("Warning: Don't forget to take your fish for a walk")
-// console.error("Error: Failed to load the part that needs to be loaded")
+let billAmount = prompt('Bill Amount:')
+billAmount = number(billAmount)
 
-// console.log('string')
-// console.log(10)
-// console.log(true)
-// console.log([1,2,3])
-// console.log(typeof 10.5, typeof 'hi', typeof true, typeof [1,2,3])
+let tipPercentage = prompt('Tip Percentage')
+tipPercentage = number(tipPercentage)
+tipPercentage *=.01
+tipPercentage = tipPercentage*billAmount
 
-// const schoolName = 'Portland High School'
-// let middleSchool = 'King Middle School'
-// console.log(schoolName)
-// console.log(middleSchool)
+let totalPrice = billAmount+tipPercentage
 
-// middleSchool = 'Lyman Moore Middle School'
-
-// console.log(schoolName)
-// console.log(middleSchool)
-
-// alert('sorry')
-
-const user_name = 'Orion'//prompt('Name:')
-const favorite_food = 'Pizza'//prompt('Favorite food:')
-
-console.log('---- User Profile ----')
-console.log('Name:', user_name)
-console.log("Favorite Food:", favorite_food)
-
-// alert('Thanks '+user_name+', enjoy')
+let priceMessage = `-- Bill Amount --
+Before tip: ${billAmount}
+Tip: ${tipPercentage}
+Total: ${totalPrice}
+`
+console.log(priceMessage)
